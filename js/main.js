173e175ff9f716568fad643d7866f9d48c2c7e6e
Aleksandr Tichiy <aleksandrpleskachevskiy@gmail.com>
2022-10-11 18:11:31 +0700

    Base table

--- js/main.js
+++ js/main.js
@@ -1,3 +1,91 @@
+// region Сортировка динамических таблиц
+document.addEventListener('DOMContentLoaded', function() {
+    const lists = [...document.querySelectorAll('.table--sortable-list')];
+    for (const list of lists) {
+        const headers = document.querySelectorAll('.table--sortable');
+        const rows = list.querySelectorAll('.table--sortable-item');
+
+        // Направление сортировки
+        const directions = Array.from(headers).map(function(header) {
+            return '';
+        });
+
+        // Преобразовать содержимое данной ячейки в заданном столбце
+        const transform = function(index, content) {
+            // Получить тип данных столбца
+            const type = headers[index].getAttribute('data-type')
+            const pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+            console.log(content)
+            switch (type) {
+                case 'number':
+                    return parseFloat(content);
+                case 'date':
+                    return new Date(content.replace(pattern,'$3-$2-$1'));
+                case 'string':
+                default:
+                    return content;
+            }
+        };
+
+        const sortColumn = function(index) {
+            // Получить текущее направление
+            const direction = index === 0 ? (directions[index] || 'desc') : (directions[index] || 'asc');
+
+            // Фактор по направлению
+            const multiplier = (direction === 'asc') ? 1 : -1;
+
+            const newRows = Array.from(rows);
+
+            //Снимаем активность ячейки
+            for (let i = 0; i < headers.length; i++){
+                headers[i].classList.remove('--active')
+                headers[i].setAttribute('data-direction', 'asc')
+            }
+
+            //Меняем направление svg
+            headers[index].classList.add('--active')
+            headers[index].setAttribute('data-direction', direction)
+
+            let data = 'data-name';
+            if(headers[index].getAttribute('data-type') === 'date') data = 'data-date'
+
+            newRows.sort(function(rowA, rowB) {
+                const cellA = rowA.getAttribute(data);
+                const cellB = rowB.getAttribute(data);
+
+                const a = transform(index, cellA);
+                const b = transform(index, cellB);
+
+                switch (true) {
+                    case a > b: return 1 * multiplier;
+                    case a < b: return -1 * multiplier;
+                    case a === b: return 0;
+                }
+            });
+
+            // Удалить старые строки
+            [].forEach.call(rows, function(row) {
+                list.removeChild(row);
+            });
+
+            // Поменять направление
+            directions[index] = direction === 'asc' ? 'desc' : 'asc';
+
+            // Добавить новую строку
+            newRows.forEach(function(newRow) {
+                list.appendChild(newRow);
+            });
+        };
+
+        [].forEach.call(headers, function(header, index) {
+            header.addEventListener('click', function() {
+                sortColumn(index);
+            });
+        });
+    }
+});
+// endregion
+
 /* Domcontent Load Start */
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener('click', function (e) {
